b9bd5e5d8ea119727f4f16eef79f89e30973680e
Nicholas Pettas <npettas@gmail.com>
2015-11-10 08:57:33 -0800

    Initial Commit

---
+++ Game/GameViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GameViewController.swift
+//  Game
+//
+//  Created by Nicholas Pettas on 11/10/15.
+//  Copyright (c) 2015 Nicholas Pettas. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let scene = GameScene(fileNamed: "GameScene") {
+            // Configure the view.
+            let skView = self.view as! SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .AspectFill
+            
+            skView.presentScene(scene)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+}
