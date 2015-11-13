802fc352b814613d751d8e0bcde82c0a3528dfae
Nicholas Pettas <npettas@gmail.com>
2015-11-12 21:56:22 -0800

    Hide status bar on iphones and ipads

---
+++ Game/GameIphoneViewController.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Game
+//
+//  Created by Nicholas Pettas on 11/12/15.
+//  Copyright © 2015 Nicholas Pettas. All rights reserved.
+//
+
+import Foundation
+
+class GameIphoneViewController: GameViewController {
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
